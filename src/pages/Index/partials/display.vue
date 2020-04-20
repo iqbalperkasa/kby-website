0f14b006d209a7734c8dea1ff60e890274cbeaf9
Iqbal Perkasa <iqbalperkasa@gmail.com>
2020-04-20 09:11:43 +0700

    fix method

--- src/pages/Index/partials/display.vue
+++ src/pages/Index/partials/display.vue
@@ -44,7 +44,7 @@ export default {
       this.collected = window.localStorage.getItem('last_collected');
     },
     fetchCollected() {
-      return fetch(`${getApiUrl()}/collected`)
+      return fetch(`${this.apiUrl()}/collected`)
         .then(resp => resp.json())
         .then(collected => {
           this.collected = collected;
