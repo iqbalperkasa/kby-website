e5a00c9a9b33f235cd4f26ccfc5f9aa3e1b943df
Iqbal Perkasa <iqbalperkasa@gmail.com>
2020-04-30 06:27:54 +0700

    relative api url

--- src/javascripts/api/index.js
+++ src/javascripts/api/index.js
@@ -1,5 +1,5 @@
 const isDev = process.env.NODE_ENV === 'development';
-const API_URL = isDev ? (process.env.DEV_API || '') : process.env.PROD_API;
+const API_URL = isDev ? (process.env.DEV_API || '') : '/api';
 
 module.exports = {
   getPhotos: () => fetch(`${API_URL}/galleries`).then(resp => resp.json()),
