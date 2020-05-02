6937b3a5bb742cc4c441203d214825c5044436d8
Iqbal Perkasa <iqbalperkasa@gmail.com>
2020-05-03 05:09:19 +0700

    add shimmer

--- src/pages/Galleries/Index.vue
+++ src/pages/Galleries/Index.vue
@@ -2,6 +2,16 @@
   section
     LayoutDefault
       h1.heading.mb-30.text-align-center Galeri
+      div(v-if='!images.length')
+        .thumbnail.shimmer
+        .thumbnail.shimmer
+        .thumbnail.shimmer
+        .thumbnail.shimmer
+        .thumbnail.shimmer
+        .thumbnail.shimmer
+        .thumbnail.shimmer
+        .thumbnail.shimmer
+        .thumbnail.shimmer
       div(v-for='(img, i) in images')
         .thumbnail(
           :style='`background-image: url(${img.src})`'
@@ -62,4 +72,15 @@ export default {
     content ''
     display block
     padding-bottom 100%
+
+.shimmer
+  background linear-gradient(90deg, rgba(204,204,204,1) 0%, rgba(238,238,238,1) 50%, rgba(204,204,204,1) 100%)
+  animation shimmer 2s infinite linear
+  background-size 1000px
+
+@keyframes shimmer
+  0%
+    background-position -1000px 0
+  100%
+    background-position 1000px 0
 </style>
